3858de78f39f96d0635bca2cdbd28cb217dcb277
Riki Fridrich <riki@fczbkk.com>
2020-10-15 17:46:27 +0200

    reagovanie na klikanie, ziskanie hodnoty

--- game.js
+++ game.js
@@ -1,4 +1,19 @@
-window.addEventListener('load', function () {
-    console.log('document sa natiahol, mame k dispozicii DOM')
-    console.log(document.body)
-})
+function startGame () {
+    let randomNumber = getRandomNumber(0, 100)
+    console.log(randomNumber)
+
+    let buttonElement = document.querySelector('#guessButton')
+    let inputElement = document.querySelector('#guessInput')
+
+    buttonElement.addEventListener('click', function () {
+        console.log('Oklikal si tlacitko.')
+        let guessedNumber = Number(inputElement.value)
+        console.log(guessedNumber, typeof guessedNumber)
+    })
+}
+
+function getRandomNumber (min, max) {
+    return min + Math.floor(Math.random() * (max - min + 1))
+}
+
+window.addEventListener('load', startGame)
