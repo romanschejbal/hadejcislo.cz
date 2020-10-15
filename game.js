94cd6c61d72e60ac0019873378ef52e7161a7c76
Riki Fridrich <riki@fczbkk.com>
2020-10-15 19:01:14 +0200

    pridanie min a max tipu

--- game.js
+++ game.js
@@ -1,11 +1,17 @@
 function startGame () {
     let randomNumber = getRandomNumber(0, 100)
+    let minRange = 0
+    let maxRange = 100
+
     console.log(randomNumber)
 
     let buttonElement = document.querySelector('#guessButton')
     let inputElement = document.querySelector('#guessInput')
     let messageElement = document.querySelector('#guessMessage')
     let formElement = document.querySelector('.guessForm')
+    let minRangeElement = document.querySelector('#minRange')
+    let maxRangeElement = document.querySelector('#maxRange')
+    let mysterElement = document.querySelector('#mysteryNumber')
 
     inputElement.focus()
 
@@ -16,12 +22,23 @@ function startGame () {
         
         if (guessedNumber === randomNumber) {
             messageElement.innerHTML = 'Gratulki! Uhadla si.'
+            minRange = guessedNumber
+            maxRange = guessedNumber
+            mysterElement.innerHTML = guessedNumber
         } else if (guessedNumber < randomNumber) {
             messageElement.innerHTML = 'Myslim na VYSSIE cislo.'
+            if (minRange < guessedNumber) {
+                minRange = guessedNumber
+            }
         } else if (guessedNumber > randomNumber) {
             messageElement.innerHTML = 'Myslim na NIZSIE cislo.'
+            if (maxRange > guessedNumber) {
+                maxRange = guessedNumber
+            }
         }
 
+        minRangeElement.innerHTML = minRange
+        maxRangeElement.innerHTML = maxRange
     }
 
     formElement.addEventListener('submit', function (event) {
